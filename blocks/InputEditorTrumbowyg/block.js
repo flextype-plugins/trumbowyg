7da1d67b59b039b181af269d9ad4967cd27aef0a
Awilum <sergey.romanenko@flextype.org>
2021-05-04 17:01:11 +0300

    feat(js): cleanup and refactor js for editor

--- blocks/InputEditorTrumbowyg/block.js
+++ blocks/InputEditorTrumbowyg/block.js
@@ -1,35 +1,3 @@
-$.trumbowyg.svgPath = $('input[name=trumbowyg-icons-path]').val();
-$('.js-html-editor').trumbowyg({
-    btnsDef: {
-        image: {
-            dropdown: [
-                'insertImage', 'noembed'
-            ],
-            ico: 'insertImage'
-        }
-    },
-    btns: [
-        [
-            'undo', 'redo'
-        ],
-        ['formatting'],
-        [
-            'strong', 'em', 'del'
-        ],
-        ['link'],
-        ['image'],
-        [
-            'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'
-        ],
-        [
-            'unorderedList', 'orderedList'
-        ],
-        ['table'],
-        ['removeformat'],
-        ['fullscreen'],
-        ['viewHTML']
-    ],
-    lang: $('input[name=trumbowyg-locale]').val(),
-    autogrow: false,
-    removeformatPasted: true
+$(".js-trumbowyg").each(function() { 
+    $(this).trumbowyg(JSON.parse($('input[name=' + $(this).prop('name') + '-trumbowyg-options]').val()));  
 });
