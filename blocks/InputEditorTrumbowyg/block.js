cc951dca93b423456d763ba831959dd9c4dce6db
Awilum <sergey.romanenko@flextype.org>
2021-05-13 11:56:12 +0300

    feat(js): use new prefixed input names

--- blocks/InputEditorTrumbowyg/block.js
+++ blocks/InputEditorTrumbowyg/block.js
@@ -1,3 +1,3 @@
 $(".js-trumbowyg").each(function() { 
-    $(this).trumbowyg(JSON.parse($('input[name=' + $(this).prop('name') + '-trumbowyg-options]').val()));  
+    $(this).trumbowyg(JSON.parse($('input[name=__trumbowyg_options_' + $(this).prop('name') + ']').val()));  
 });
