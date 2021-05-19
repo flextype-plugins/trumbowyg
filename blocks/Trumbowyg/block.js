ceb62fe083535a64286c108c80bdd1cee4aea7e8
Awilum <sergey.romanenko@flextype.org>
2021-05-19 22:57:42 +0300

    feat(blocks): store trumbowyg object in window

--- blocks/Trumbowyg/block.js
+++ blocks/Trumbowyg/block.js
@@ -1,3 +1,4 @@
 $(".js-trumbowyg").each(function() { 
-    $(this).trumbowyg(JSON.parse($('input[name=__trumbowyg_options_' + $(this).prop('name') + ']').val()));  
+    var trumbowyg = $(this).prop('name');
+    window[trumbowyg] = $(this).trumbowyg(JSON.parse($('input[name=__trumbowyg_options_' + trumbowyg + ']').val()));  
 });
